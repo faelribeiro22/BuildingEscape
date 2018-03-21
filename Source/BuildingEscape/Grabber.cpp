f3c9613a6a150c37e4bacf4808ea2159e1680f50
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2018-03-21 03:06:09 -0300

    format px

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -2,7 +2,10 @@
 
 #include "Grabber.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,6 +33,37 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	// TODO Log out to test
+	/*
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Position: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString());
+	*/
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	// Draw a red trace in the world to visualise
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+
+	// Ray-cast out to reach distance
+
+	// See what we hit
 }
 
